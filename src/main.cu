a7a14ba2d36c75dd813c7c855275d5648fcab27c
Tom Boudard <tom.boudard@grenoble-inp.org>
2023-11-24 09:41:31 +0100

    Code : Read file init

---
+++ src/main.cu
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+struct point2D{
+
+    float x;
+    float y;
+}
+
+void readFile(string nameFile){
+    vector<point2D> pointsVector;
+
+    std::ifstream inputFile;
+    inputFile.open(nameFile);
+    
+    while(){
+        
+    }
+
+    pointsVector.push_back().
+}
+
+int main{
+
+    return 0;
+}
