97d530df8e73891f8b97a96de98a7c76e2ec94f8
Thibaut Coudroy <thibaut.coudroy@grenoble-inp.org>
2023-12-15 09:52:57 +0100

    multiple projection in parallel

--- src/projection.cu
+++ src/projection.cu
@@ -1,17 +1,18 @@
 #define N 1024 // TODO WHICH VALUE?
 
-__global__ void projectPoint(point2D *pts, point2D *projected, long unsigned int refIdx, bool onAxisX) {
-    point2D refPoint = pts[refIdx];
+__global__ void projectPoint(point2D *pts, point2D *projected, long unsigned int nbPts, bool onAxisX) {
     long unsigned int idx = (blockIdx.x*N + threadIdx.x);
+    long unsigned int projectionIdx = idx / ((nbPts+N-1)/N);
+    long unsigned int refIdx = (nbPts * (1 + 2*projectionIdx)) / (2*N);
+
+    point2D refPoint = pts[refIdx];
     point2D localPoint = pts[idx];
     float delta_y = refPoint.y - localPoint.y;
     float delta_x = refPoint.x - localPoint.x;
 
     projected[idx].index = localPoint.index;
-    // x takes delta between y values
-    projected[idx].x = onAxisX ? delta_y : delta_x;
-    // y takes euclidian distance squared between points
-    projected[idx].y = delta_y * delta_y + delta_x * delta_x;
+    projected[idx].x = onAxisX ? delta_y : delta_x; // x takes delta between values over an axis
+    projected[idx].y = delta_y * delta_y + delta_x * delta_x; // y takes euclidian distance squared between points
 }
 
 point2D* projection(point2D* pointsOnGPU, long unsigned int nbPts) {
@@ -21,9 +22,9 @@ point2D* projection(point2D* pointsOnGPU, long unsigned int nbPts) {
     point2D* pointsProjected;
     cudaMalloc((void **)&pointsProjected, mem);
     
-    dim3 dimGrid((nbPts+N-1)/N, 1);   // Nb of blocks
-    dim3 dimBlock(N, 1);
-    projectPoint<<<dimGrid, dimBlock>>>(pointsOnGPU, pointsProjected, nbPts/2, true);
+    int dimGrid = (nbPts+N-1)/N;   // Nb of blocks
+    int dimBlock = N;
+    projectPoint<<<dimGrid, dimBlock>>>(pointsOnGPU, pointsProjected, nbPts, true);
 
     cudaDeviceSynchronize();
 
